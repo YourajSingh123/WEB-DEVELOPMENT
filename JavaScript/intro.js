b174d5578ff381c02dcd23c7492b57718b663900
YourajSingh123 <rajyou0123@gmail.com>
2026-08-25 20:57:14 +0530

    add file

---
+++ JavaScript/intro.js
@@ -0,0 +1,19 @@
+// alert("Hello yuvi");
+
+console.log("Hello Yuvi jii");
+console.log("Yuvi is running...");
+console.log("yuvi is software developer...");
+console.log("he is also well at running...");
+
+// var a = prompt("Enter the Number:");
+// console.log("the number is:" + a);
+
+// var isTrue = confirm("are you sure you want to leave the page and Blast your System");
+// if((isTrue)){
+//     console.log("System is blasting");
+// }else{
+//     console.log("System is blasting");
+// }
+
+// document.title = "Hello bhaiyo..";
+// document.body.style.backgroundColor = "yellow";
