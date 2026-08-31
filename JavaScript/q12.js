06809d6aa8c9c00dba679aabb8714474c8a267e9
YourajSingh123 <rajyou0123@gmail.com>
2026-08-31 16:24:04 +0530

    add file

---
+++ JavaScript/q12.js
@@ -0,0 +1,13 @@
+console.log("Exercise 12");
+
+document.getElementById("yuv").style.backgroundColor = "orange";
+document.getElementById("kh").style.backgroundColor = "red";
+document.getElementById("om").style.backgroundColor = "blue";
+document.getElementById("ay").style.backgroundColor = "green";
+document.getElementById("aa").style.backgroundColor = "black";
+
+document.getElementById("yuv").style.color = "blue";
+document.getElementById("kh").style.color = "black";
+document.getElementById("om").style.color = "orange";
+document.getElementById("ay").style.color = "red";
+document.getElementById("aa").style.color = "white";
